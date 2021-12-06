f14a3ec6b0c4e1c703bc02da3b598a5b64ce113d
goatz18 <jg852@njit.edu>
2021-12-05 19:57:46 -0500

    Edit: Unfinished CSV coding

---
+++ tests/csv_test.py
@@ -0,0 +1,10 @@
+from calc.calculations.addition import Addition
+from calc.calculations.subtraction import Subtraction
+from calc.calculations.multiplication import Multiplication
+from calc.calculations.division import Division
+import pandas as pd
+data = pd.read_csv("../fileutilities/addition.csv")
+data = pd.read_csv("../fileutilities/subtraction.csv")
+data = pd.read_csv("../fileutilities/multiplication.csv")
+data = pd.read_csv("../fileutilities/division.csv")
+

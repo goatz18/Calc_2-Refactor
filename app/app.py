4660a8fe007563ca11e81036cc658c6be98860a6
goatz18 <jg852@njit.edu>
2021-12-12 05:52:22 -0500

    Edit: Adapting Old Project HTML Into New Project for Bootstratp Formatting.

--- app/app.py
+++ app/app.py
@@ -1,6 +1,10 @@
 """A simple flask web app"""
 from flask import Flask, flash, render_template
 from app.controllers.index_controller import IndexController
+from app.controllers.index_controller import BehindTheWebController
+from app.controllers.index_controller import SearchEngineWarsController
+from app.controllers.index_controller import OopOopController
+from app.controllers.index_controller import OopSelect
 from app.controllers.calculator_controller import CalculatorController
 from werkzeug.debug import DebuggedApplication
 
@@ -19,6 +23,22 @@ def calculator_get():
 @app.route("/calculator", methods=['POST'])
 def calculator_post():
     return CalculatorController.post()
+
+@app.route("/behindtheweb", methods=['GET'])
+def behind_the_web_get():
+    return BehindTheWebController.get()
+
+@app.route("/search", methods=['GET'])
+def search_get():
+    return SearchEngineWarsController.get()
+
+@app.route("/oopoop", methods=['GET'])
+def oopoop_get():
+    return OopOopController.get()
+
+@app.route("/oopselect", methods=['GET'])
+def oopselect_get():
+    return OopSelect.get()
 
 #@app.route("/index")
 #def flash_me():
--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -5,3 +5,23 @@ class IndexController(ControllerBase):
     @staticmethod
     def get():
         return render_template('index.html')
+
+class BehindTheWebController(ControllerBase):
+    @staticmethod
+    def get():
+        return render_template('behindtheweb.html')
+
+class SearchEngineWarsController(ControllerBase):
+    @staticmethod
+    def get():
+        return render_template('search.html')
+
+class OopOopController(ControllerBase):
+    @staticmethod
+    def get():
+        return render_template('oopoop.html')
+
+class OopSelect(ControllerBase):
+    @staticmethod
+    def get():
+        return render_template('oopselect.html')
